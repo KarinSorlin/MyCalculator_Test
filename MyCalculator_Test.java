9727acd12ca64a15d72123becd310962f5613867
Karin Sörlin <sorlin.karin@gmail.com>
2018-11-26 10:02:06 +0100

    added a simple test example

--- MyCalculator_Test.java
+++ MyCalculator_Test.java
@@ -1,3 +1,20 @@
 public class MyCalculator_Test{
+  private static MyCalculator calc = new MyCalculator();
 
-}
+  private static boolean test_StartValueOffFirstNumber(){
+    double temp = calc.getFirstNumber();
+    if(10 == temp){
+      return true;
+    }
+    return false;
+  }
+
+  public static void main(String[] args){
+    if(test_StartValueOffFirstNumber()){
+      System.out.println("test_StartValueOffFirstNumber: " + "PASSED");
+    } else{
+      System.out.println("test_StartValueOffFirstNumber: " + "FAILED");
+    }
+
+  } //main
+} //class
